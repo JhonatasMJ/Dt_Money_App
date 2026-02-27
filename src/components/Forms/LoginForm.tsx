7e08b577b8a4125b027912d1261c1449a20f7f60
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-02-27 00:10:16 -0300

    feat: Login Screen

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { formLoginParams } from "../../types/LoginParams";
 import { Input } from "../Input";
 import Button from "../Button";
+import { Text, View } from "react-native";
 
 export function LoginForm() {
   const {
@@ -26,8 +27,18 @@ export function LoginForm() {
       leftIconName="lock-outline"
       secureTextEntry
     />
-    <Button mode="fill" iconName="arrow-forward">
+
+    <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
+    <Button iconName="arrow-forward">
       Login
     </Button>
+    <View>
+      <Text className="mb-6 text-gray-300 text-base">Ainda não possui uma conta?</Text>
+      <Button mode="outline" iconName="arrow-forward">
+      Cadastrar
+    </Button>
+    </View>
+    </View>
+ 
   </>;
 }
